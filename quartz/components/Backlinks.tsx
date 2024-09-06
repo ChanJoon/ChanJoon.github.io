8b7c359b6545ca74b6968dee3ab4dec6f3841fd0
ChanJoon <as06047@yonsei.ac.kr>
2024-09-06 20:45:23 +0900

    Quartz sync: Sep 6, 2024, 8:45 PM

--- quartz/components/Backlinks.tsx
+++ quartz/components/Backlinks.tsx
@@ -25,7 +25,7 @@ const Backlinks: QuartzComponent = ({
             </li>
           ))
         ) : (
-          <li>{i18n(cfg.locale).components.backlinks.noBacklinksFound}</li>
+          <li></li>
         )}
       </ul>
     </div>
